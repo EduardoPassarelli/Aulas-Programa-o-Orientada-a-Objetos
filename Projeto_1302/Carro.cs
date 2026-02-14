5fbcba2706614200bd8108b1098de117b87a3f58
0220482412003 <0220482412003@LAB4MAQ13>
2026-02-13 21:14:53 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_1302/Carro.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_1302
+{
+    public class Carro
+    {
+        //Características - Propriedades/Atributos
+        public string? marca;
+        public string? modelo;
+        public int ano;
+        public string? cor;
+        public string? placa;
+        public bool flex;
+
+        //Ações - Métodos
+
+        public void ligarCarro()
+        {
+            Console.WriteLine("O carro está ligado!");
+        }
+
+        public void desligarCarro()
+        {
+            Console.WriteLine("O carro está desligado!");
+        }
+
+        public void acelerarCarro()
+        {
+            Console.WriteLine("Carro acelerando...");
+        }
+
+        public void frearCarro()
+        {
+            Console.WriteLine("Carro freiando");
+        }
+
+        public void mostrarDetalhesCarro()
+        {
+            Console.WriteLine("Informe os dados do carro");
+
+            Console.WriteLine("Modelo: ");
+            modelo = Console.ReadLine();
+
+            Console.WriteLine("Marca: ");
+            marca = Console.ReadLine();
+
+            Console.WriteLine("Ano: ");
+            ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Placa: ");
+            placa = Console.ReadLine();
+
+            Console.WriteLine("Cor: ");
+            cor = Console.ReadLine();
+
+            Console.WriteLine("-------Dados do Carro-------");
+            Console.WriteLine(modelo);
+            Console.WriteLine(marca);
+            Console.WriteLine(ano);
+            Console.WriteLine(placa);
+            Console.WriteLine(cor);
+            Console.WriteLine("----------------------------");
+
+        }
+    }
+}
---
+++ Projeto_1302/Program.cs
@@ -0,0 +1,10 @@
+﻿using Projeto_1302;
+
+//Criação de um objeto
+Carro meuCarro = new Carro();
+Carro carroEsportivo = new Carro();
+Carro carroAntigo = new Carro();
+
+
+carroEsportivo.mostrarDetalhesCarro();
+carroAntigo.mostrarDetalhesCarro();
